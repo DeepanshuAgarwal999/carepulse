97bc5531453e1b95f0383b41b1fa020b749d2378
deepanshu agarwal <deepanshuagarwal9999@gmail.com>
2024-07-24 09:52:32 +0530

    fix conditions

--- src/app/admin/Admin.tsx
+++ src/app/admin/Admin.tsx
@@ -13,28 +13,31 @@ const AdminInfo = ({ appointments }: any) => {
     if (!encryptedKey) {
         router.push('/')
     }
-    return (
-        <div className='flex flex-col mx-auto max-w-7xl gap-y-14'>
-            <header className='admin-header'>
-                <Link href={'/'} className='cursor-pointer'>
-                    <Image src='/assets/icons/logo-full.svg' height={32} width={162} alt='logo' className='h-8 w-fit' />
-                </Link>
-                <p className='text-16-semibold'>Admin Dashboard</p>
-            </header>
-            <main className='admin-main'>
-                <section className='w-full space-y-4'>
-                    <h1 className='header'>Welcome 👋</h1>
-                    <p className='text-dark-700'>Start the day with managing new appointments</p>
+    if (encryptedKey) {
+        return (
+            <div className='flex flex-col mx-auto max-w-7xl gap-y-14'>
+                <header className='admin-header'>
+                    <Link href={'/'} className='cursor-pointer'>
+                        <Image src='/assets/icons/logo-full.svg' height={32} width={162} alt='logo' className='h-8 w-fit' />
+                    </Link>
+                    <p className='text-16-semibold'>Admin Dashboard</p>
+                </header>
+                <main className='admin-main'>
+                    <section className='w-full space-y-4'>
+                        <h1 className='header'>Welcome 👋</h1>
+                        <p className='text-dark-700'>Start the day with managing new appointments</p>
+                    </section>
+                </main>
+                <section className='admin-stat px-2'>
+                    <StatCard type="appointments" count={appointments.scheduledCount} label="Scheduled appointments" icons="/assets/icons/appointments.svg" />
+                    <StatCard type="cancelled" count={appointments.cancelledCount} label="cancelled appointments" icons="/assets/icons/cancelled.svg" />
+                    <StatCard type="pending" count={appointments.pendingCount} label="pending appointments" icons="/assets/icons/pending.svg" />
                 </section>
-            </main>
-            <section className='admin-stat px-2'>
-                <StatCard type="appointments" count={appointments.scheduledCount} label="Scheduled appointments" icons="/assets/icons/appointments.svg" />
-                <StatCard type="cancelled" count={appointments.cancelledCount} label="cancelled appointments" icons="/assets/icons/cancelled.svg" />
-                <StatCard type="pending" count={appointments.pendingCount} label="pending appointments" icons="/assets/icons/pending.svg" />
-            </section>
-            <DataTable data={appointments.documents} columns={columns} />
-        </div>
-    )
+                <DataTable data={appointments.documents} columns={columns} />
+            </div>
+        )
+    }
+    return null;
 }
 
 export default AdminInfo
